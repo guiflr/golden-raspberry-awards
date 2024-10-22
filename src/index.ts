7eb728098262eada6630ab81284252b98f793cc6
guilherme <guiflr97@gmail.com>
2024-10-22 16:02:01 -0300

    refactor: remove explicit disconnect call and remove all data when run seed data

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import fs from 'fs';
 import csv from 'csv-parser';
-import { prisma } from './database/prisma';
 import { formatMovieData } from './utils/format-film-data';
-import { createMovie } from './repositories/movie-repository';
+import { createMovie, deleteMovies } from './repositories/movie-repository';
 
 async function processCSV(filePath: string) {
     fs.createReadStream(filePath)
         .pipe(csv({
             separator: '\t'
         }))
-        .on('data', (headerWtiData: any) => {
+        .on('data', async (headerWtiData: any) => {
             const movie = formatMovieData(headerWtiData)
-            createMovie(movie);
+            await createMovie(movie);
         })
         .on('end', async () => {
             console.log('All movies was inserted.');
-            await prisma.$disconnect();
         });
 }
 
-processCSV('movielist.csv');
+deleteMovies().then(() => {
+    processCSV('movielist.csv');
+})
+
--- src/repositories/movie-repository.ts
+++ src/repositories/movie-repository.ts
@@ -10,3 +10,7 @@ export async function createMovie(movie: Movie) {
 export function getWinners() {
     return prisma.film.findMany({ where: { winner: 'YES' }, orderBy: { year: 'desc' } })
 }
+
+export async function deleteMovies() {
+    return await prisma.film.deleteMany()
+}
